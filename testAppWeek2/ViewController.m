c3df3b7f899bf877a769975b0a107468998c33ff
Jimmy Kim <jimmykim925@fullsail.edu>
2012-08-03 18:49:02 -0700

    initial files added

---
+++ testAppWeek2/ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  testAppWeek2
+//
+//  Created by Jimmy Kim on 8/3/12.
+//  Copyright (c) 2012 Jimmy Kim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    
+    
+    [super viewWillAppear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+      return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+  } else {
+      return YES;
+  }
+}
+
+@end
