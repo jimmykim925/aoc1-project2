e929201f04cebc8e9904df8cdb4fca58558079b5
Jimmy Kim <jimmykim925@fullsail.edu>
2012-08-07 16:27:32 -0700

    added files

---
+++ testAppWeek2/ViewController.h
@@ -0,0 +1,36 @@
+//
+//  ViewController.h
+//  testAppWeek2
+//
+//  Created by Jimmy Kim on 8/3/12.
+//  Copyright (c) 2012 Jimmy Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+
+  // Create pointer to UILabels
+  UILabel *title;
+  UILabel *author;
+  UILabel *name;
+  UILabel *published;
+  UILabel *publishedDate;
+  UILabel *summary;
+  UILabel *summaryDetails;
+  UILabel *listOfItems;
+  UILabel *listDetails;
+  
+  // Create pointers to string items
+  NSString *listItem0;
+  NSString *listItem1;
+  NSString *listItem2;
+  NSString *listItem3;
+  NSString *listItem4;
+  NSMutableString *listItemMutableString;
+  NSArray *listItemsArray;
+  NSMutableArray *mutableArray;
+}
+
+@end
